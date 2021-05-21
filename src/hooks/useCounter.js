d8eb725016983bb93a8d8b6f5f26f784d221a8b9
Luis Carrion <lcarrionalvarez@gmail.com>
2021-05-21 15:52:05 -0500

    brakingBad Quotes added

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -2,22 +2,22 @@ import { useState } from "react"
 
 export const useCounter = (inicialState = 10 ) => {
 
-  const [state, setState] = useState(inicialState)
+  const [counter, setCounter] = useState(inicialState)
 
-  const increment = (factor = 2) => {
-    setState( state + factor )
+  const increment = () => {
+    setCounter( counter + 1 )
   }
 
-  const decrement = (factor = 2) => {
-    setState( state - factor)
+  const decrement = () => {
+    setCounter( counter - 1)
   }
 
   const reset = () => {
-    setState( inicialState )
+    setCounter( inicialState )
   }
   
   return {
-    state,
+    counter,
     increment,
     decrement,
     reset
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { FormWothCustomHook } from './components/02-useEffect/FormWothCustomHook';
+// import { FormWothCustomHook } from './components/02-useEffect/FormWothCustomHook';
+import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { CounterApp } from './components/01-useState/CounterApp';
 /* import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
  */
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
-    <FormWothCustomHook />,
+    <MultipleCustomHooks />,
   document.getElementById('root')
 );
