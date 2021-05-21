c7233e117226b9fd3c6fbf9df3d1e33041a0243d
Luis Carrion <lcarrionalvarez@gmail.com>
2021-05-21 17:57:15 -0500

    usRef basic example

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { FormWothCustomHook } from './components/02-useEffect/FormWothCustomHook';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 // import { CounterApp } from './components/01-useState/CounterApp';
-/* import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
- */
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
-    <MultipleCustomHooks />,
+    <FocusScreen />,
   document.getElementById('root')
 );
