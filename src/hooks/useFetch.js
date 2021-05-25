4caeed30f16b95781e328e7376c11a9a4dc8a176
Luis Carrion <lcarrionalvarez@gmail.com>
2021-05-24 20:01:56 -0500

    added the use of memo function

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const useFetch = ( url ) => {
   const [state, setState] = useState({data: null, loading: true, error: null})
+
+  const isMounted = useRef(true) //
+
+  useEffect( () => { // Cuando este efecto se desmonte, cambia el estado de referencia
+    return () => { // no re-renderiza el componente, simplemente mantiene una referencia
+      isMounted.current = false // basicamente, es falso cuando deja de existir
+    }
+  }, [])
 
   useEffect( () => {
 
@@ -10,11 +18,13 @@ export const useFetch = ( url ) => {
     fetch( url )
       .then( resp => resp.json() )
       .then( data => {
-        setState ({
-          loading: false,
-          error: null,
-          data
-        })
+        if (isMounted.current) {
+          setState ({ // gracias a las validaciones antes hechas con el use ref, se puede hacer
+            loading: false, // un trabajo condicional con este componente, ya que se puede
+            error: null, // trabajar de manera segura si esta montado, y no necesariamente montarlo
+            data // y llamar el useState, si no se encuentra montado
+          })
+        }  
       })
   }, [url])
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { FormWothCustomHook } from './components/02-useEffect/FormWothCustomHook';
 // import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+import { Memorize } from './components/06-memos/Memorize';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 
 ReactDOM.render(
-    <FocusScreen />,
+    <Memorize />,
   document.getElementById('root')
 );
